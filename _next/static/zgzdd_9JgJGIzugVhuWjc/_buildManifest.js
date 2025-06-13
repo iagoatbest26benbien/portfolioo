b4235c3171d064610e84a66a7c7d1ebd1e6299f0
El Manssouri Anas <anaselmanssouri479@gmail.com>
2025-06-14 00:22:35 +0200

    Deploy to gh-pages

--- _next/static/zgzdd_9JgJGIzugVhuWjc/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/cb355538-3018662dd27a7cd2.js","static/chunks/pages/index-6cdf48b9b1bcdb21.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
